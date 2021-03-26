6e3d83db9e4cc5e641cc0e5ad55ce1131847dbdf
Ralf <ralfvb@outlook.com>
2021-03-26 11:50:43 +0100

    Genesis

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learn_key_vault
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
