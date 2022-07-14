83d7673554825d621e2226c9e7e645841440fdcd
Ralf <ravonbro@microsoft.com>
2022-07-14 10:53:24 +0200

    Updated to .NET 6

--- Program.cs
+++ Program.cs
@@ -8,17 +8,16 @@ namespace learn_key_vault
     {
         static void Main(string[] args)
         {
-            // <authenticate>
-            
+
+            // Authenticate with Azure Key Vault
+
             Console.Write("What's the name of your Key Vault?");
             string keyVaultName = Console.ReadLine();
             
             var kvUri = "https://" + keyVaultName + ".vault.azure.net";
             var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
             
-            // </authenticate>
-
-            // <add secret>
+            // Add a secret to the Key Vault
 
             Console.Write("Input the name of your secret > ");
             string secretName = Console.ReadLine();
@@ -32,15 +31,11 @@ namespace learn_key_vault
 
             Console.WriteLine(" done.");
             
-            // </add secret>
-
-            // <read secret>
+            // Get a secret from the Key Vault
 
             KeyVaultSecret secret = client.GetSecret(secretName);
             
             Console.WriteLine(string.Format("Your secret value read from the vault: {0}", secret.Value.ToString()));
-
-            // </read secret>
         }
     }
 }
